0edc9624fd696b372736f0963c7668ec3247c564
Landen Sisk <58862240+LandenSisk@users.noreply.github.com>
2022-11-02 21:00:43 -0500

    Add memory limit to build file

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ dd if=boot_sect.bin of=LandOS.flp bs=512 seek=0 conv=notrunc
 dd if=kernel.bin of=LandOS.flp bs=512 seek=0 conv=notrunc
 rm boot_sect.bin
 rm kernel.bin
-qemu-system-x86_64 -fda LandOS.flp
+qemu-system-x86_64 -fda LandOS.flp -m 1M
